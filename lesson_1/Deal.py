0dc804e8fbb9636384d713de7662671933ddaf87
Calvin Wong <calwong42991@gmail.com>
2018-08-30 19:53:43 -0700

    Completed function for Deal

---
+++ lesson_1/Deal.py
@@ -0,0 +1,8 @@
+import random 
+
+def deal(numHands, n=5, deck = [r+s for r in '23456789TJQKA' for s in 'SHDC']):
+  "Shuffle the deck and deal out num_hands n-card hands"
+  random.shuffle(deck)
+  return [deck[n*i:n*(i+1)] for i in range(numHands)]
+
+print(deal(2, 7))
