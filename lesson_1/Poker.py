142ff0f7562afe215db3ed291c17baf8b87e23c3
Calvin Wong <calwong42991@gmail.com>
2018-08-30 18:59:12 -0700

    Added card Rank test

--- lesson_1/Poker.py
+++ lesson_1/Poker.py
@@ -14,11 +14,15 @@ def test():
   sf = '6C 7C 8C 0C TC'.split()
   fk = '9D 9H 9S 9C 7D'.split()
   fh = 'TD TC TH 7C 7D'.split()
-
+  assert card_ranks(sf) == [10, 9, 8, 7, 6]
+  assert card_ranks(sf) == [9, 9, 9, 9, 7]
+  assert card_ranks(sf) == [10, 10, 10, 7, 7]
   assert poker([sf, fk, fh]) == sf
   assert poker([fk, fh]) == fk
   assert poker([fh, fh]) == fh
   assert poker([sf]) == sf
   assert poker([sf] + 99*[fh]) == sf
-
+  assert hand_rank(sf) == (8, 10)
+  assert hand_rank(fk) == (7, 9, 7)
+  assert hand_rank(fh) == (6, 10, 7)
   return 'tests pass'
