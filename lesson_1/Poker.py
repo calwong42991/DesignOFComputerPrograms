be47c45bf5c0f767684a2e4030db015f0168088d
Calvin Wong <calwong42991@gmail.com>
2018-08-30 18:57:55 -0700

    added test

--- lesson_1/Poker.py
+++ lesson_1/Poker.py
@@ -8,3 +8,17 @@ def poker(hands):
 
 def hand_rank(hand):
   "Return a value indicating the ranking of a hand."
+
+def test():
+  "Test cases for the functions in poker program."
+  sf = '6C 7C 8C 0C TC'.split()
+  fk = '9D 9H 9S 9C 7D'.split()
+  fh = 'TD TC TH 7C 7D'.split()
+
+  assert poker([sf, fk, fh]) == sf
+  assert poker([fk, fh]) == fk
+  assert poker([fh, fh]) == fh
+  assert poker([sf]) == sf
+  assert poker([sf] + 99*[fh]) == sf
+
+  return 'tests pass'
