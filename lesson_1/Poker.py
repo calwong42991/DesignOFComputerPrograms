9e649ff07960551ecc7f95cbbddc4ef0f00057f6
Calvin Wong <calwong42991@gmail.com>
2018-08-30 19:16:04 -0700

    added test for kind

--- lesson_1/Poker.py
+++ lesson_1/Poker.py
@@ -14,12 +14,13 @@ def card_ranks(cards):
 
 def straight(ranks):
   "Return true if the ordered ranks from a 5-card straight"
-  return (max(ranks) - min(ranks) == 4) and len(set(ranks)) === 5
+  return (max(ranks) - min(ranks) == 4) and len(set(ranks)) == 5
 
 def flush(hand):
   "Return True if all the cards have the same suit"
   suits = [s for r,s in hand]
   return len(set(suits)) == 1
+
 
 def hand_rank(hand):
   "Return a value indicating the ranking of a hand."
@@ -48,6 +49,17 @@ def test():
   sf = '6C 7C 8C 0C TC'.split()
   fk = '9D 9H 9S 9C 7D'.split()
   fh = 'TD TC TH 7C 7D'.split()
+  fh = 'TD TC TH 7C 7D'.split()
+  tp = '5S 5D 9H 9C 6S'.split()
+  fkranks = card_ranks(fk)
+  tpranks = card_ranks(tp)
+  assert kind(4, fkranks) == 9
+  assert kind(3, fkranks) == None
+  assert kind(2, fkranks) == None
+  assert kind(1, fkranks) == 7
+  two_pair(fkranks) == None
+  two_pair(tpranks) == (9, 5)
+
   assert straight([9, 8, 7, 6, 5]) == True
   assert straight([9, 8, 8, 6, 5]) == False
   assert flush(sf) == True
