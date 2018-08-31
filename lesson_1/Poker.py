8f89dae2843d022163bc43df7e2e43acf51c885a
Calvin Wong <calwong42991@gmail.com>
2018-08-30 18:59:52 -0700

    Added Hand_rank

--- lesson_1/Poker.py
+++ lesson_1/Poker.py
@@ -8,6 +8,25 @@ def poker(hands):
 
 def hand_rank(hand):
   "Return a value indicating the ranking of a hand."
+  ranks = card_ranks(hand)
+  if straight(ranks) and flush(hand):
+    return (8, max(ranks))
+  elif kind(4, ranks):
+    return (7, kind(4, ranks), kind(1, ranks))
+  elif kind(3, ranks) and kind(2, ranks):
+    return (6, kind(3, ranks), kind(2, ranks))
+  elif flush(hand):
+    return (5, ranks)
+  elif straight(ranks):
+    return (4, max(ranks))
+  elif kind(3, ranks):
+    return (3, kind(3, ranks), ranks)
+  elif two_pair(ranks):
+    return (2, two_pair(ranks), ranks)
+  elif kind(2, ranks):
+    return (1, kind(2, ranks), ranks)
+  else: 
+    return (0, ranks)
 
 def test():
   "Test cases for the functions in poker program."
