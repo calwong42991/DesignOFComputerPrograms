06f361d23cfbc0925f9a50f9689f5cc34bf24216
Calvin Wong <calwong42991@gmail.com>
2018-08-30 18:54:02 -0700

    Max review

---
+++ lesson_1/Max.py
@@ -0,0 +1,8 @@
+def poker(hands):
+  "return the best hand: poker([hand, ...]) => hand"
+  # takes list as input and prints highest number
+  return max
+
+# example
+print(max([3, 4, 5, 0])) # 5
+print(max([3, 4, -5, 0], key=abs))  
